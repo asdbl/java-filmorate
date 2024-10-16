45acb4268913d10d314c030eccf79eae9f0f34aa
AndreyKlyonin <andreyklyonin@gmail.com>
2024-10-16 21:49:13 +0300

    Удалить неиспользуемые зависимости. Исправить аннотации в Film.

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,6 +1,8 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import ru.yandex.practicum.filmorate.validation.ReleaseDate;
@@ -16,12 +18,11 @@ public class Film {
     private Long id;
     @NotBlank(message = "Имя не может быть пустым")
     private String name;
+    @NotBlank
     @Size(max = 200, message = "Мксимальня длина описания - 200 символов")
     private String description;
-    @NotNull(message = "Дата выхода не может быть пустой")
     @ReleaseDate(year = 1895, month = 12, day = 28, message = "Дата выхода фильма не может быть раньше 1895-12-28")
     private LocalDate releaseDate;
-    @NotNull(message = "Длительность фильма не может быть пустой")
     @Positive(message = "Длительность фильм не может быть отрицательной")
     private int duration;
 }
